9e9a97802971c982fc98c57bc707ebbd76fe558f
aspachkovsky <pbl.symbolist@gmail.com>
2014-12-11 16:36:56 +0300

    task 2 modifications

--- synch/task2.cpp
+++ synch/task2.cpp
@@ -11,47 +11,91 @@
 struct sembuf sem_buf_wakeup = {0, 1, 0};
 struct sembuf sem_buf_wait = {0, -1, 0};
  
-int main(void) {
-	char c, tmp;
-	key_t shm_key = 1234;
-	char *s;	
+int sem_id;
+char* shmem;
+int shm_key = 1234;
 
- 	int shm_id = shmget(shm_key, shm_size, IPC_CREAT | 0666); // create
- 	if (shm_id < 0) {
-		printf("Server: shget failed!");
-		return 1;
-	}
+#if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
+/* union semun is defined by including <sys/sem.h> */
+#else
+/* according to X/OPEN we have to define it ourselves */
+union semun {
+      int val;                  /* value for SETVAL */
+      struct semid_ds *buf;     /* buffer for IPC_STAT, IPC_SET */
+      unsigned short *array;    /* array for GETALL, SETALL */
+                                /* Linux specific part: */
+      struct seminfo *__buf;    /* buffer for IPC_INFO */
+};
+#endif
  
-	char* shmem = (char *) shmat(shm_id, 0, 0); // attach
-	if (shmem == (char *) -1) {
-		printf("Server: shmat failed!");
-		return 1;
+void client(char* message) {
+	
+	while (1) {
+		semop(sem_id, &sem_buf_wait, 1);
+		
+		if (strlen(shmem) == 0) {
+			strcpy(shmem, message);
+		}
+		
+		semop(sem_id, &sem_buf_wakeup, 1);
+		sleep(1);
 	}
+}
 
-	int sem_key = 9999;
-	int sem_id = semget(sem_key, 1, IPC_CREAT | 0660);
-
-	if (sem_id == -1) {
-		printf("Server: semget failed!");
-		return 1;
-	} 
+ 
+void server() {
+	
+	union semun semopts;
+	semopts.val = 1;
+	semctl(sem_id, 0, SETVAL, semopts);
 
 	memset(shmem, 0, shm_size);
 
 	while (1) {
 		semop(sem_id, &sem_buf_wait, 1);
-
 		if (strlen(shmem) > 0) {
-			printf("Server received: %s\n", shmem);
+			printf("Server received: %s \n", shmem);
 			memset(shmem, 0, shm_size);
 		}		
-		
 		semop(sem_id, &sem_buf_wakeup, 1);
 	}
- 
-	if(shmdt(shmem) != 0) {
-		printf("Server: shmdt failed!\n");
-	} 
+}
 
+int main(int argc, char* argv[]) {
+	
+	int shm_id = shmget(shm_key, shm_size, IPC_CREAT | 0666); // create
+ 	if (shm_id < 0) {
+		printf("shget failed! \n");
+		return 1;
+	}
+ 
+	shmem = (char *) shmat(shm_id, 0, 0); // attach
+	if (shmem == (char *) -1) {
+		printf("shmat failed! \n");
+		return 1;
+	}
+	
+	int sem_key = 9999;
+	sem_id = semget(sem_key, 1, IPC_CREAT | 0660);
+	if (sem_id == -1) {
+		printf("semget failed!");
+		return 1;
+	} 
+	
+	if (argc == 1) {
+		printf("Launched as Server. Listening to clients... \n");
+		server();
+	} else if (argc == 2) {
+		printf("Launched as Client. Sending messages to the server... \n");
+		client(argv[1]);
+	} else {
+		printf("Wrong number of arguments. \n");
+		return 1;
+	}
+	
+	if(shmdt(shmem) != 0) {
+		printf("shmdt failed!\n");
+	} 
+	
 	return 0;
 }
